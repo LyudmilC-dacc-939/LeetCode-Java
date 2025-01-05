6188f861357a426a0d4bde9e8605bcd931596617
LAC_Knightmare <Whats_with_lusk0@abv.bg>
2025-01-05 15:51:29 +0200

    reverse words in a sentence pt. 2

--- src/ReverseWordsInASentence.java
+++ src/ReverseWordsInASentence.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class ReverseWordsInASentence {
@@ -28,7 +30,47 @@ public class ReverseWordsInASentence {
 
         String input = scanner.nextLine();
 
-        System.out.println("You entered: " + input);
+        List<String> wordsAsList = new ArrayList<>();
+        String currentWord = "";
+
+        for (int i = 0; i < input.trim().length(); i++) {
+            String currentChar = String.valueOf(input.trim().charAt(i));
+
+            if (currentChar.matches("\\s")) {
+                if (!currentWord.isEmpty()) {
+                    //this if condition IS necessary as when we have consecutive whitespace it will will save it in the
+                    //arrayList and we want to avoid that
+                    wordsAsList.add(currentWord);
+                    currentWord = "";
+                }
+            } else {
+                currentWord = "%s%s".formatted(currentWord, currentChar);
+            }
+
+            // If this is the last character of the string, add the final word
+            if (i == input.trim().length() - 1 && !currentWord.isEmpty()) {
+                wordsAsList.add(currentWord);
+            }
+        }
+
+       /* Wrong solution(need it as a reminder) :
+          for (int i = 0; i <= input.trim().length() - 1; i++) {
+                    String currentChar = String.valueOf(input.trim().charAt(i));
+                    if(currentChar.matches("\s") || i == input.trim().length() - 1){
+                        wordsAsList.add(currentWord);
+                        currentWord = "";
+                    }
+                    currentWord = "%s%s".formatted(currentWord, currentChar);
+                }*/
+
+        List<String> reversedWords = new ArrayList<>();
+        for (int i = wordsAsList.size() - 1; i >= 0; i--) {
+            reversedWords.add(wordsAsList.get(i));
+        }
+
+        String result = String.join(" ", reversedWords);
+        System.out.println("Your string reversed: " + result);
+
 
         scanner.close();
     }
