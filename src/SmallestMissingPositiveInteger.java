4d3750ffea163305d30aaa57b450935b5f99fe83
LAC_Knightmare <Whats_with_lusk0@abv.bg>
2025-01-10 23:59:53 +0200

    smallest missing positive Integer pt. 2

--- src/SmallestMissingPositiveInteger.java
+++ src/SmallestMissingPositiveInteger.java
@@ -1,3 +1,6 @@
+import java.util.HashSet;
+import java.util.Set;
+
 public class SmallestMissingPositiveInteger {
 
     //Hard: Smallest Missing Positive Integer
@@ -18,20 +21,38 @@ public class SmallestMissingPositiveInteger {
     //Solve it in O(n) time and constant space.
 
     public static void main(String[] args) {
-        int[] arr = {3, 4, -1, 1};
+        int[] arr = {3, 4, -1, 17, 1, 2};
+        int smallestMissingPositiveNumber = 1;
+        boolean isSmallestPositiveInt = false;
+
+        Set<Integer> setOfNums = new HashSet<>();
+        for (int num : arr) {
+            setOfNums.add(num);
+        }
+
+        do {
+            if (setOfNums.contains(smallestMissingPositiveNumber)) {
+                smallestMissingPositiveNumber++;
+            }
+            isSmallestPositiveInt = true;
+        } while (!isSmallestPositiveInt);
+      /*  //Wrong Solution:
         int smallestPositiveNumber = 0;
+        int secondSmallestPositiveNumber = 0;
+        int smallestMissingPositiveNumber;
 
         for (int i = 0; i < arr.length; i++)
             for (int j = i + 1; j < arr.length; j++) {
-                if (i < j && i > 0) {
-                    smallestPositiveNumber = i;
-                } else if (i >= j && i > 0){
-                    smallestPositiveNumber = j;
+                if (arr[i] < arr[j] && arr[i] > 0 && arr[j] > 0) {
+                    smallestPositiveNumber = arr[i];
+                    secondSmallestPositiveNumber = arr[j];
+                } else {
+                    smallestPositiveNumber = arr[j];
+                    secondSmallestPositiveNumber = arr[i];
                 }
 
-            }
+            }*/
 
-        System.out.println(smallestPositiveNumber);
     }
 
 }
