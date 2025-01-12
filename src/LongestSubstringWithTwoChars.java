16d8ed831f6566ed392c3404013baff8d773b4b8
LAC_Knightmare <Whats_with_lusk0@abv.bg>
2025-01-12 23:58:10 +0200

    longest substring with two chars pt. 2

--- src/LongestSubstringWithTwoChars.java
+++ src/LongestSubstringWithTwoChars.java
@@ -1,5 +1,4 @@
-import java.util.HashSet;
-import java.util.Set;
+import java.util.*;
 
 public class LongestSubstringWithTwoChars {
 
@@ -20,20 +19,35 @@ public class LongestSubstringWithTwoChars {
     //s consists of English letters only.
 
     public static void main(String[] args) {
-        String s = "eceba";
+        String s = "eeccebaec";
 
         char[] stringToCharArray = s.toCharArray();
-        Set<Character> uniqueCharacters = new HashSet<>();
+        Set<Character> uniqueCharacters = new LinkedHashSet<>();
+        List<Character> listOfUsableCharacters = new ArrayList<>();
         StringBuilder finalString = new StringBuilder();
 
         for (char ch : stringToCharArray) {
             uniqueCharacters.add(ch);
-            finalString.append(ch);
-            if(uniqueCharacters.size() == 2){
-                break;
+        }
+        if (!uniqueCharacters.isEmpty()) {
+            listOfUsableCharacters.add(uniqueCharacters.stream().toList().get(0));
+            if (uniqueCharacters.size() > 1) {
+                listOfUsableCharacters.add(uniqueCharacters.stream().toList().get(1));
             }
         }
 
+        for (char ch : stringToCharArray) {
+            if (listOfUsableCharacters.contains(ch)) {
+                finalString.append(ch);
+            }
+            if (!s.contains(finalString)) {
+                finalString.delete(finalString.length()-1);
+            }
+        }
+
+
         System.out.println(finalString.length());
+        System.out.println(finalString);
     }
 }
+
