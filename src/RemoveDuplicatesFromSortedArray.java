ad4c32613b532e6e5b832a5862f72f65ef033d36
LAC_Knightmare <Whats_with_lusk0@abv.bg>
2025-01-18 23:59:27 +0200

    remove duplicates from sorted array

--- src/RemoveDuplicatesFromSortedArray.java
+++ src/RemoveDuplicatesFromSortedArray.java
@@ -1,8 +1,14 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+
 public class RemoveDuplicatesFromSortedArray {
 
     //Medium 1: Remove Duplicates from Sorted Array II
     //Problem:
-    //Given a sorted array nums, remove duplicates in place such that each element appears at most twice and return the new length of the array.
+    //Given a sorted array nums, remove duplicates in place such that each element appears at most twice and return
+    // the new length of the array.
     //
     //Example:
     //
@@ -15,5 +21,25 @@ public class RemoveDuplicatesFromSortedArray {
     //
     //1 <= len(nums) <= 10^5
 
+    public static void main(String[] args) {
+        System.out.println(removeDuplicates(new int[]{1, 1, 1, 2, 2, 3}));
+    }
+
+    static List<Integer> removeDuplicates(int[] nums) {
+        int counter = 0;
+        List<Integer> listWithRemovedDuplicates = Arrays.stream(nums).boxed().toList();
+        for (int i = 0; i <= listWithRemovedDuplicates.size(); i++) {
+            for (int j = i + 1; j <= listWithRemovedDuplicates.size(); j++) {
+                if (Objects.equals(listWithRemovedDuplicates.get(i), listWithRemovedDuplicates.get(j))) {
+                    counter++;
+                }
+                if (counter == 2) {
+                    listWithRemovedDuplicates.remove(j);
+                    counter = 0;
+                }
+            }
+        }
+        return listWithRemovedDuplicates;
+    }
 
 }
